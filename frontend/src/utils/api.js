0f2be32fd94eae78e6a8ac167d60414eef9227ea
MuhammadZaryab3 <zaryabmuhammad321@gmail.com>
2026-06-27 12:41:18 +0500

    hardcode railway api url

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({ 
-  baseURL: 'courteous-appreciation-production.up.railway.app/api'
+  baseURL: 'https://courteous-appreciation-production.up.railway.app/api'
 })
 
 api.interceptors.request.use(config => {
