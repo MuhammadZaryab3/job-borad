7dc03ddab38f407b374e336b61a2410f3377bfca
MuhammadZaryab3 <zaryabmuhammad321@gmail.com>
2026-06-27 12:24:17 +0500

    fix api url

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
-const api = axios.create({ baseURL: '/api' })
+const api = axios.create({ 
+  baseURL: 'courteous-appreciation-production.up.railway.app/api'
+})
 
 api.interceptors.request.use(config => {
   const token = localStorage.getItem('token')
